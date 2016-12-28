1e6ce55edc0d9e86c59095ab00d06655bf245a76
boss <770830543@qq.com>
2016-12-28 20:43:03 +0800

    第二次修改

--- app/src/main/java/com/bwei/hanyanbing20161228/MainActivity.java
+++ app/src/main/java/com/bwei/hanyanbing20161228/MainActivity.java
@@ -3,6 +3,12 @@ package com.bwei.hanyanbing20161228;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 1.名字:韩炎兵
+ * 2.时间: 2016/12/28 20:41
+ * 3.作用：主页面的显示
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
